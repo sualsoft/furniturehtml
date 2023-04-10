58c44d4119f598ec59e961619bfe234253489653
sumiyuru priyasara <sumiyurupriyasara@gmail.com>
2023-04-10 12:20:38 +0530

    Add slider
    Change js css and html

--- assets/js/main.js
+++ assets/js/main.js
@@ -246,6 +246,39 @@
   /**
    * Clients Slider
    */
+  new Swiper(".product-slider", {
+    speed: 400,
+    loop: true,
+    autoplay: false,
+    slidesPerView: "auto",
+    pagination: {
+      el: ".swiper-pagination",
+      type: "bullets",
+      clickable: true,
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      480: {
+        slidesPerView: 3,
+        spaceBetween: 10,
+      },
+      640: {
+        slidesPerView: 4,
+        spaceBetween: 10,
+      },
+      992: {
+        slidesPerView: 4,
+        spaceBetween: 12,
+      },
+    },
+  });
+
+  /**
+   * Clients Slider
+   */
   new Swiper(".clients-slider", {
     speed: 400,
     loop: true,
